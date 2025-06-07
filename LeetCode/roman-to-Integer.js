7c1e0bf9619bb195af6185243f43c6f0bd4fa23b
Mohammad Arkan <mdarkanurl@gmail.com>
2025-06-06 22:24:31 -0800

    roman-to-Integer.js commited

---
+++ LeetCode/roman-to-Integer.js
@@ -0,0 +1,43 @@
+// Question is 👇
+// Example 1:
+
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
+// Example 2:
+
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V= 5, III = 3.
+// Example 3:
+
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+var romanToInt = function(s) {
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    
+    let total = 0;
+    let prevValue = 0;
+
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currentValue = romanMap[s[i]];
+        if (currentValue < prevValue) {
+            total -= currentValue;
+        } else {
+            total += currentValue;
+        }
+        prevValue = currentValue;
+    }
+
+    return total;
+};
